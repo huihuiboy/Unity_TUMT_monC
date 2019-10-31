9057435608155b76a75c2134f52def99e7d52e30
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2019-10-31 10:22:00 +0800

    lady動畫

--- MonC/Assets/程式設計/gogoRobot.cs
+++ MonC/Assets/程式設計/gogoRobot.cs
@@ -3,15 +3,31 @@
 public class gogoRobot : MonoBehaviour {
 
     Animator ani;
+    int count = 1;
+
 
 	void Start () {
         ani = gameObject.GetComponent<Animator>();
+        
 	}
 
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.Space))
-           ani.SetTrigger("dancee") ;
+        if (count/2 > 0 && count % 2 == 0)
+        {
+
+            ani.SetBool("dance",true);
+        }
+        else if(count % 2 != 0)
+        {
+            ani.SetBool("dance", false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            count++;
+            Debug.Log("count="+count);
+        }
         ani.SetBool("run", Input.GetKey(KeyCode.R));
 
 	}
---
+++ MonC/Assets/程式設計/lady.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lady : MonoBehaviour {
+
+    //
+    //
+    //
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk() { }
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack() { }
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump() { }
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    private void Hurt() { }
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private void Die() { }
+}
