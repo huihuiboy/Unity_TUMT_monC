a042e5b063adae01ead8a376c1f709695ba6086a
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2019-11-01 11:54:51 +0800

    機器人動畫

--- MonC/Assets/程式設計/gogoRobot.cs
+++ MonC/Assets/程式設計/gogoRobot.cs
@@ -4,31 +4,52 @@ public class gogoRobot : MonoBehaviour {
 
     Animator ani;
     int count = 1;
-
+    bool dan;
 
 	void Start () {
         ani = gameObject.GetComponent<Animator>();
-        
+        dan = false;
 	}
 
 	void Update () {
+        //利用奇偶數判斷動畫-只適用只有idle與跳舞
+        /* if (Input.GetKeyDown(KeyCode.Space) )
+         {
+             count++;
+             Debug.Log("count="+count);
+         if (count/2 > 0 && count % 2 == 0)
+         {
+             ani.SetBool("idle", false);
+             ani.SetTrigger("dancee");
+         }
+         else if(count / 2 > 0 && count % 2 != 0)
+         {
+             ani.SetBool("idle", !true);
 
-        if (count/2 > 0 && count % 2 == 0)
+         }
+         }*/
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-
-            ani.SetBool("dance",true);
+            dan = !false ;
+            Debug.Log("now"+dan);
+            if (dan == true)
+            {
+                ani.SetBool("idle", false);
+                ani.SetTrigger("dancee");
+            }
+            else if(dan == false)
+            {
+                ani.SetBool("idle", true);
+            }
         }
-        else if(count % 2 != 0)
+
+        if (Input.GetKey(KeyCode.R))
         {
-            ani.SetBool("dance", false);
-
+            ani.SetBool("run", true);
+            ani.ResetTrigger("dancee");
         }
-        if (Input.GetKeyDown(KeyCode.Space) )
-        {
-            count++;
-            Debug.Log("count="+count);
-        }
-        ani.SetBool("run", Input.GetKey(KeyCode.R));
+        else ani.SetBool("run", false);
 
-	}
+    }
 }
