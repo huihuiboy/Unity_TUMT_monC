20d7e446b90da84910ff51082614cb2d02121243
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2019-11-07 11:47:52 +0800

    run count++

--- MonC/Assets/程式設計/gogoRobot.cs
+++ MonC/Assets/程式設計/gogoRobot.cs
@@ -13,7 +13,7 @@ public class gogoRobot : MonoBehaviour {
 
 	void Update () {
         //利用奇偶數判斷動畫-只適用只有idle與跳舞
-        /* if (Input.GetKeyDown(KeyCode.Space) )
+         if (Input.GetKeyDown(KeyCode.Space) )
          {
              count++;
              Debug.Log("count="+count);
@@ -24,32 +24,26 @@ public class gogoRobot : MonoBehaviour {
          }
          else if(count / 2 > 0 && count % 2 != 0)
          {
-             ani.SetBool("idle", !true);
+             ani.SetBool("idle", true);
 
          }
-         }*/
+         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            dan = !false ;
-            Debug.Log("now"+dan);
-            if (dan == true)
-            {
-                ani.SetBool("idle", false);
-                ani.SetTrigger("dancee");
-            }
-            else if(dan == false)
-            {
-                ani.SetBool("idle", true);
-            }
-        }
+       
 
         if (Input.GetKey(KeyCode.R))
         {
             ani.SetBool("run", true);
             ani.ResetTrigger("dancee");
+
+            
         }
         else ani.SetBool("run", false);
 
+
+        if (Input.GetKeyDown(KeyCode.R) && ani.GetBool("run"))
+        {
+            count++;
+        }
     }
 }
